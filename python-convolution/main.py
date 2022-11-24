e50d06ecb79a691075eea633586776e382b034b3
Boris Zolotov <boris.a.zolotov@yandex.com>
2022-11-24 14:36:48 +0300

    setcolors

--- python-convolution/main.py
+++ python-convolution/main.py
@@ -3,26 +3,25 @@ import scipy as scp
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
+
 # Настройки графиков
 
+def setcolors(a):
+    a.set(xlabel="Month",
+           ylabel="Precipitation\n(inches)",
+           facecolor="#0f0f0f")
 
-ax = plt.axes()
+    a.set_title("Ковидные бляди", color='w')
 
-ax.set(xlabel="Month",
-       ylabel="Precipitation\n(inches)",
-       facecolor="#0f0f0f")
+    a.spines['bottom'].set_color("#0f0f0f")
+    a.spines['left'].set_color("#0f0f0f")
+    a.spines['right'].set_color("#0f0f0f")
+    a.spines['top'].set_color("#0f0f0f")
 
-ax.set_title("Ковидные бляди", color='w')
-
-ax.spines['bottom'].set_color("#0f0f0f")
-ax.spines['left'].set_color("#0f0f0f")
-ax.spines['right'].set_color("#0f0f0f")
-ax.spines['top'].set_color("#0f0f0f")
-
-ax.xaxis.label.set_color('white')
-ax.yaxis.label.set_color('white')
-ax.tick_params(axis='x', colors='white')
-ax.tick_params(axis='y', colors='white')
+    a.xaxis.label.set_color('white')
+    a.yaxis.label.set_color('white')
+    a.tick_params(axis='x', colors='white')
+    a.tick_params(axis='y', colors='white')
 
 
 # ax.set_facecolor("#0f0f0f")
@@ -45,6 +44,8 @@ res = scp.signal.fftconvolve(days, kernel)
 figure1 = plt.figure(facecolor="#0f0f0f")
 
 axes1 = figure1.subplots()
+
+setcolors(axes1)
 
 axes1.plot(res[4:8], color='w')
 
