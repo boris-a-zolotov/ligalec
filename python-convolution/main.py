101cf240f74f6378505195028500c8d5285242ba
Boris Zolotov <boris.a.zolotov@yandex.com>
2022-11-24 15:58:19 +0300

    ylim; generate images

--- python-convolution/main.py
+++ python-convolution/main.py
@@ -10,6 +10,7 @@ dplot = '#96e6ff'
 def setaxes(a, ttl, xl, yl):
     a.set(xlabel=xl, ylabel=yl, facecolor=dgray)  # заголовок, подписи осей, цвет внутри рамок
     a.set_title(ttl, color='w')
+    a.set_ylim(bottom=0)  # фиксированный низ оси y — на нуле
 
     a.spines['bottom'].set_color(dgray)  # цвета рамок графика
     a.spines['left'].set_color(dgray)
@@ -27,22 +28,45 @@ def unitalize(arr: np.ndarray) -> np.ndarray:  # сделать массив с 
     return arr / npsum
 
 
-days = [7, 7, 7, 1, 1, 1, 1]
+pandDuration = 105
 
-kernel = [0, 0, 1, 1, 1]
+days = np.zeros(pandDuration, dtype=float)
+samp = np.zeros(pandDuration, dtype=float)
+
+for x in range(pandDuration):
+    days[x] = 2500 + 1000 * np.arctan(0.08 * (x - 75))
+    samp[x] = 2500 + 1000 * np.arctan(0.08 * (x - 75))
+
+for w in range(pandDuration // 7):
+    defic = days[7 * w + 2] - 400
+    days[7 * w + 2] = 400
+    days[7 * w + 3] += defic / 2
+    days[7 * w + 4] += defic * 0.4
+    days[7 * w + 5] += defic / 10
+
+for x in range(pandDuration):
+    days[x] += max(np.random.normal(0, 400), -1 * days[x])
+
+kernel = np.full(7, 1, dtype=float)
 
 kernel = unitalize(kernel)
 
-res = scp.signal.fftconvolve(days, kernel)
+wavg = scp.signal.fftconvolve(days, kernel)
 
-figure1 = plt.figure(facecolor=dgray, figsize=(8.2, 4.1))
 
-axes1 = figure1.subplots()
+outarray = [[0, days[6:], dplot, 'Заболеваемость: сырые данные'],
+            [1, wavg[6:-6], '#ebaf9b', 'Заболеваемость: недельное среднее'],
+            [2, samp[6:], '#73f587', 'Недельное среднее и тренд']]
 
-setaxes(axes1, 'ковид', 'дни', 'значения')
+for s in outarray:
+    figure = plt.figure(facecolor=dgray, figsize=(8.2, 4.1))
+    axes = figure.subplots()
 
-axes1.plot(res[4:8], color=dplot)
+    for k in range(s[0] + 1):
+        axes.plot(outarray[k][1], color=outarray[k][2])
 
-pdf1 = PdfPages("Figures.pdf")
-pdf1.savefig(figure1)
-pdf1.close()
+    setaxes(axes, s[3], 'дни', 'значения')
+
+    pdf = PdfPages("covid-%d.pdf" % (s[0]))
+    pdf.savefig(figure)
+    pdf.close()
