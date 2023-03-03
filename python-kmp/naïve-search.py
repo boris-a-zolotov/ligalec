b9bca46e52aa6a46bc40c132335ebb3f6037773c
Boris Zolotov <boris.a.zolotov@yandex.com>
2023-03-03 16:28:25 +0300

    рисунок про наивный алгоритм

--- python-kmp/naïve-search.py
+++ python-kmp/naïve-search.py
@@ -8,8 +8,14 @@ inStr = "1212121212"
 scStr = "12123"
 
 for i in range(len(inStr)):
-    outStr += outstring(i, 0, "white", inStr[i])
+    outStr += outstring(i, 0.5, "white", inStr[i])
 
+for i in range(len(inStr) - len(scStr) + 1):
+    for j in range(len(scStr)):
+        if scStr[j] == inStr[i + j]:
+            outStr += outstring(i + j, -i - 1, "white", scStr[j])
+        else:
+            outStr += outstring(i + j, -i - 1, "white,opacity=0.45", scStr[j])
 
 f = open("intikz.tex", "w")
 f.write(outStr)
